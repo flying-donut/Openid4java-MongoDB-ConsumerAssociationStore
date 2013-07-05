34dfa0d19678f5f5dbc23963e24837718f2bd679
Dimitrios Souflis <dsouflis@acm.org>
2013-07-05 15:12:15 +0300

    Added O.S. license to the Java sources

--- src/main/java/com/flyingdonut/implementation/helpers/MongoDbConsumerAssociationStore.java
+++ src/main/java/com/flyingdonut/implementation/helpers/MongoDbConsumerAssociationStore.java
@@ -12,10 +12,10 @@ import org.slf4j.LoggerFactory;
 import java.util.Date;
 
 /**
- * Created with IntelliJ IDEA.
- * User: d.souflis
- * Date: 25/4/2013
- * Time: 10:24 μμ
+ * Copyright 2013 Flying Donut P.C.
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this work except in compliance with the License. You may obtain a copy of the License in the LICENSE file, or at:
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
  */
 public class MongoDbConsumerAssociationStore implements ConsumerAssociationStore {
     protected final Logger logger = LoggerFactory.getLogger(this.getClass());
